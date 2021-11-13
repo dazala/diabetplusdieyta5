1edb67ef8b7877e405c8518b42e674b09970cce2
dazala <alexeyweb90@gmail.com>
2021-11-13 17:56:12 +0200

    Initial Commit

---
+++ diabetplusdieyta5/diabetplusdieyta5App.swift
@@ -0,0 +1,17 @@
+//
+//  diabetplusdieyta5App.swift
+//  diabetplusdieyta5
+//
+//  Created by alexey on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct diabetplusdieyta5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
